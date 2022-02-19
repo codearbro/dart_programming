3958544e41939b2980e23c03729b07bd2e44f3c9
suraj verma <46342901+codearbro@users.noreply.github.com>
2022-02-20 01:00:47 +0530

    Create 2_comment.dart

---
+++ 2_comment.dart
@@ -0,0 +1,22 @@
+void main()
+{
+  print("\t\t##  COMMENTS IN DART PROGRAMMING LANGUAGE  ##\n\n");
+  print("This program is all about comments in dart programming language:-\n");
+  //this is the comment used in dart programming language
+  
+  print("1.Comments are the sentences that ignored by the compiler");
+  print("2.Comments used to explain the method of the program ");
+  print("3.Comments can only read by the programmers");
+  print("4.Comments will increase the readability of program");
+  print("5.There are in dart programming language two types of comments\n");
+  print("\t1.Single line comments\n\t2.Multi line comments");
+  print("\n1. Single line comment uses // for from start from the sentence");
+  
+  //SINGLE LINE COMMENT
+  print("\n2.Multiline comments use /* from start and */ in end of comment");
+  /* this 
+   * is 
+   * multiline
+   * comment*/
+  print("\n\t\tTHANK YOU");
+}
