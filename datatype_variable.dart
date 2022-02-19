b8904c0866f47aeaaeabf9d9b8d0310512499450
suraj verma <46342901+codearbro@users.noreply.github.com>
2022-02-20 04:57:27 +0530

    Create datatype_variable.dart

---
+++ datatype_variable.dart
@@ -0,0 +1,26 @@
+void main()
+{
+  print("\t##  DATA TYPE AND VARIABLE  ##");
+  
+  //number
+  int a=20;
+  var aa=40;
+  //decimal point number
+  double b=3.4;
+  var ba=34.60;
+  //string
+  String c="myName";
+  var ca="yourName";
+  //boolean
+  bool isOkay=true;
+  var notOkay=false;
+  
+  print(a);
+  print(aa);
+  print(b);
+  print(ba);
+  print(c);
+  print(ca);
+  print(isOkay);
+  print(notOkay);
+}
